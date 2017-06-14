4528bd9b3996f66d9f5fa6f7b5e2ba8b67763a78
derry <deyi.li2015@gmail.com>
2017-06-14 15:59:36 -0700

    ex8

--- ex7/ex7/kMeansInitCentroids.m
+++ ex7/ex7/kMeansInitCentroids.m
@@ -1,4 +1,4 @@
-function centroids = kMeansInitCentroids(X, K)
+sfunction centroids = kMeansInitCentroids(X, K)
 %KMEANSINITCENTROIDS This function initializes K centroids that are to be 
 %used in K-Means on the dataset X
 %   centroids = KMEANSINITCENTROIDS(X, K) returns K initial centroids to be
